46d7adb489a1c8522670b02c5fe74a6416ebd246
Alexey Zhiburtovich <alexeycitadel@mail.ru>
2020-12-13 20:20:44 +0300

    My tests implemented

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,16 +1,16 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class SimpleTests {
     @Test
-    void successTest() {
-        assertEquals(2 * 2, 4);
+    void positiveTest() {
+        assertEquals(2 + 2, 4);
     }
 
     @Test
     void negativeTest() {
-        assertEquals(2 * 2, 5);
-
+        assertNotEquals(2 + 2, 5);
     }
 }
