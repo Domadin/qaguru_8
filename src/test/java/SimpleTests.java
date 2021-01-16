c952f4e84b3b1a65232a5e31b9a6aff92e60a7f3
Alexey Zhiburtovich <alexeycitadel@mail.ru>
2021-01-16 19:06:28 +0300

    Allure added

--- src/test/java/SimpleTests.java
+++ src/test/java/SimpleTests.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -5,16 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class SimpleTests {
     @Test
+    @DisplayName("Этот тест пройдёт")
     void positiveTest() {
         assertEquals(2 + 2, 4);
     }
 
     @Test
+    @DisplayName("Этот тест упадёт")
     void negativeTest() {
-        assertNotEquals(2 + 2, 5);
+        assertEquals(2 + 2, 5);
     }
 
     @Test
+    @DisplayName("Этот тест отключёт")
+    @Disabled("Отключен потому что я так повелел")
     void someTest() {
         assertNotEquals(3 + 3, 6);
     }
